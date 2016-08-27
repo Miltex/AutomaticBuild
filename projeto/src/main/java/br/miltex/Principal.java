455d9ed828a4c12858f1aa7d01f6557d0b4ca77e
Miltex <flmiltex@gmail.com>
2016-08-27 20:13:38 -0300

    Testando o build

---
+++ projeto/src/main/java/br/miltex/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.miltex;
+
+/**
+ * @author Milton
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+}
